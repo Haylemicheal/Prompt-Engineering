4845f230d39ac834ac3d39da26a26db76ce94d6b
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-09-17 16:27:26 +0300

    Write dataloader

---
+++ scripts/dataloader.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import json
+class DataLoader:
+    def read_xls(self, path):
+        """A method for reading excel files
+        Args:
+            path: path of the file
+        Returns:
+            df: pandas data fream
+        """
+        df = pd.read_excel(path) 
+        return df
+    def read_json(self, path):
+        """A method for reading json file
+        Args:
+            path: Path of the json file
+        Returns:
+            data: json file
+        """
+        with open('../data/relations_dev.txt') as f:
+            data = json.load(f)
+        return data
