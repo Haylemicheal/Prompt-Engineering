df1a29fd25ebeeeb8b86979a75c331071caa8fe9
Haylemicheal Berihun <haylemicheal.mekonnen@gmail.com>
2022-09-17 16:25:16 +0300

    Modify the bnewsscore

--- templates/main.py
+++ templates/main.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..')))
+from scripts.news_classifier_pipline import Pipeline
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('hello.html')
+   return render_template('index.html')
+
+@app.route('/news')
+def news():
+   return render_template('news.html')
+
+@app.route('/bnewscore')
+def newsscore():
+   title = request.args.get('title')
+   desc = request.args.get('desc')
+   pipe = Pipeline()
+   pred = pipe.pipeline(title, desc)
+   return render_template('news.html', prediction=pred)
+
+@app.route('/jdentities')
+def job_desc_entities():
+   return render_template('job_desc_ent.html')
+
 
 if __name__ == '__main__':
    app.run(debug = True)
